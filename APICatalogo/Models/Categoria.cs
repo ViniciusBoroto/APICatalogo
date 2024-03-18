b218369342950fb05dbff02bd56b6fe8e19e3c65
Vinícius <viniciusdosanjos63@gmail.com>
2024-03-17 23:12:23 -0300

    DB migrations and data annotations complete

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,8 +1,25 @@
-﻿namespace APICatalogo.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 }
