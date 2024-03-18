f696754c9ed022a56ef9667a9ddbdab976f26c28
Vinícius <viniciusdosanjos63@gmail.com>
2024-03-18 16:22:51 -0300

    ProdutosController Creation

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,16 @@
+﻿using APICatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDBContext _context;
+
+    public ProdutosController(AppDBContext context)
+    {
+        _context = context;
+    }
+}
